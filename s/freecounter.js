2dbd26c096a2ffda38d076f7180fe6d49a18d296
Patrick Sy <patrick.sy@get-it.us>
2022-02-25 06:28:17 +0100

    working counter and horrible css for squarespace...

--- s/freecounter.js
+++ s/freecounter.js
@@ -1,12 +1,33 @@
 
-function freeSince(el) {
+function freeSince(startDateEl,
+  counterEl = {
+    years: "",
+    months: "",
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: ""
+  },
+  targetSelectFunc) {
 
-  let stringDate = el.textContent;
+  const updateCountdown = (ts) => {
+    new Map(Object.entries(counterEl))
+      .forEach((v, k) => {
+        const target = targetSelectFunc(v);
 
-  let date = Date.parse(stringDate);
+        if (target) {
+          const num =  String(ts[k]).padStart(2, '0')
+          target.innerHTML = num;
+        }
+      });
+  }
 
-  let timerId = countdown(date, ts => el.innerHTML = ts);
+  const stringDate = startDateEl.textContent;
 
-  console.debug(timerId);
+  const date = Date.parse(stringDate);
+
+  const timerId = countdown(date, updateCountdown);
+
+  console.debug(`Running coundown with ${timerId}`);
 
 }
